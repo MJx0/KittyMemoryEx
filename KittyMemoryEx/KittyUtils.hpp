11625d09be29ece4e9746e808c9f64def7c00939
MJ <32413318+MJx0@users.noreply.github.com>
2023-10-24 00:24:45 -0700

    added page offset & len macros

--- KittyMemoryEx/KittyUtils.hpp
+++ KittyMemoryEx/KittyUtils.hpp
@@ -39,6 +39,8 @@
 #define KT_PAGE_SIZE (sysconf(_SC_PAGE_SIZE))
 #define KT_PAGE_START(x) (uintptr_t(x) & ~(KT_PAGE_SIZE - 1))
 #define KT_PAGE_END(x) (KT_PAGE_START(x + KT_PAGE_SIZE - 1))
+#define KT_PAGE_OFFSET(x) (uintptr_t(x) - KT_PAGE_START(x))
+#define KT_PAGE_LEN(x) (size_t(KT_PAGE_SIZE - KT_PAGE_OFFSET(x)))
 
 #if defined(__ANDROID__) && defined(kUSE_LOGCAT)
 
