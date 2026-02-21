f460b643d0a43079afd249ae48c269f4cb0b97ab
MJ <32413318+MJx0@users.noreply.github.com>
2026-02-21 08:01:37 +0000

    improve compilation

--- example-android/Android.mk
+++ example-android/Android.mk
@@ -3,16 +3,21 @@ LOCAL_PATH := $(call my-dir)
 KITTYMEMORY_PATH = ../KittyMemoryEx
 KITTYMEMORY_SRC = $(wildcard $(KITTYMEMORY_PATH)/*.cpp)
 
+# Disable keystone support
+KT_DISABLE_KEYSTONE ?= false
+
 ## Keystone static lib link
+ifeq ($(KT_DISABLE_KEYSTONE),false)
 include $(CLEAR_VARS)
 LOCAL_MODULE    := Keystone
 LOCAL_SRC_FILES := $(KITTYMEMORY_PATH)/Deps/Keystone/libs-android/$(TARGET_ARCH_ABI)/libkeystone.a
 include $(PREBUILT_STATIC_LIBRARY)
+endif
 
 ## Example exec
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := KittyMemoryExExample
+LOCAL_MODULE := KittyMemoryExample
 
 # add -DkITTYMEMORY_DEBUG for debug outputs
 LOCAL_CPPFLAGS += -std=c++17
@@ -20,6 +25,8 @@ LOCAL_CPPFLAGS += -std=c++17
 LOCAL_SRC_FILES := example.cpp $(KITTYMEMORY_SRC)
 
 ## add keystone
+ifeq ($(KT_DISABLE_KEYSTONE),false)
 LOCAL_STATIC_LIBRARIES := Keystone
+endif
 
 include $(BUILD_EXECUTABLE)
